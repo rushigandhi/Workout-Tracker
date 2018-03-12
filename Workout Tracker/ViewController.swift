36f9b909936d096fde96a95cc2511d5f570a8c50
rushigandhi <rushi.gandhi@hotmail.com>
2018-03-12 01:59:48 -0400

    Started Project

---
+++ Workout Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Workout Tracker
+//
+//  Created by Rushi Gandhi on 2018-03-12.
+//  Copyright © 2018 Rushi Gandhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
