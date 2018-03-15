74068b82b98cb369de73dabf6dec42dae3666987
rushigandhi <rushi.gandhi@hotmail.com>
2018-03-15 00:35:04 -0400

    (add): Image to Layout

--- Workout Tracker/ViewController.swift
+++ Workout Tracker/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+//        view.backgroundColor = .black
+        let weightsImage = UIImageView(image: #imageLiteral(resourceName: "weights"))
+        view.addSubview(weightsImage)
+        
+        // this enables autolayout for our weightsImage
+        weightsImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        weightsImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        weightsImage.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        weightsImage.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+
+
+        
     }
 
     override func didReceiveMemoryWarning() {
